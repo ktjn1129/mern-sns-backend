9821cc0ea4546c747cc0f9cb467b5be470afe94a
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2023-12-23 20:05:25 +0900

    add post function

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //投稿を作成する
 router.post("/", async (req, res) => {
@@ -9,6 +10,94 @@ router.post("/", async (req, res) => {
     return res.status(200).json(savedPost);
   } catch (err) {
     return res.status(500).json(err);
+  }
+});
+
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功しました！");
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を編集できません");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//投稿を削除する
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("投稿を削除しました");
+    } else {
+      return res.status(403).json("あなたは他の人の投稿を削除できません");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//投稿の取得
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//投稿にいいねを押す／外す
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //まだ投稿にいいねが押されていない場合
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json("投稿にいいねを押しました"); //投稿にすでにいいねが押されている場合
+    } else {
+      await post.updateOne({
+        $pull: {
+          $pull: {
+            likes: req.body.userId,
+          },
+        },
+      });
+      return res.status(200).json("投稿からいいねを外しました");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//タイムラインの投稿を取得する
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+
+    //フォローしている人の投稿内容を全件取得する
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
