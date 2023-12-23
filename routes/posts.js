755178e25bdcc215528e855bfdc883b090b96372
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2023-12-23 14:39:32 +0900

    first commit

---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+//投稿を作成する
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users.js");
+const authRoute = require("./routes/auth.js");
+const postRoute = require("./routes/posts.js");
+const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//データベース接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DBと接続中・・・");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//ミドルウェア
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));
